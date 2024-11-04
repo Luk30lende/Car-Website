c3ecb359772e414176989b0f0ccf159d368219e9
Luke Olende <olendeluke2k@gmail.com>
2024-11-04 16:41:39 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,12 +28,12 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader() {
+const scrollHeader = () => {
   const header = document.getElementById("header");
-  //  When scroll is greater than 50 viewport height, add scroll-header class
-  if (this.scrollY >= 50) header.classList.add("scroll-header");
-  else header.classList.remove("scroll-header");
-}
+  window.scrollY >= 50 
+    ? header.classList.add("scroll-header") 
+    : header.classList.remove("scroll-header");
+};
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== POPULAR SWIPER ===============*/
@@ -77,7 +77,33 @@ function activeFeatured(){
 linkFeatured.forEach(l => l.addEventListener('click', activeFeatured))
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  window.scrollY >= 350 
+    ? scrollUp.classList.add('show-scroll') 
+    : scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current=>{
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
